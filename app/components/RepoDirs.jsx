6a585e44abbb5ea9df2467f1e7dcaebcbf59b1a7
Han-Enc <h01062598017@gmail.com>
2023-05-31 12:34:49 +0900

    5

---
+++ app/components/RepoDirs.jsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+
+async function fetchRepoContents(name) {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  const response = await fetch(
+    `https://api.github.com/repos/Han-Enc/${name}/contents`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+  const contents = await response.json();
+  return contents;
+}
+
+const RepoDirs = async ({ name }) => {
+  const contents = await fetchRepoContents(name);
+  const dirs = contents.filter((content) => content.type === 'dir');
+
+  return (
+    <>
+      <h3>Directories</h3>
+      <ul>
+        {dirs.map((dir) => (
+          <li key={dir.path}>
+            <Link
+              href={`https://www.github.com/Han-Enc/${name}/tree/master/${dir.path}`}
+            >
+              {dir.path}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+export default RepoDirs;
